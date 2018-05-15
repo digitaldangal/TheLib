c104908fcd53870a30be121433f4d92e0074c93b
ruben <rubenbustos33@gmail.com>
2018-05-15 19:14:54 -0400

    Add babel

---
+++ .next/bundles/pages/index.js
@@ -0,0 +1,47 @@
+module.exports =
+
+        __NEXT_REGISTER_PAGE('/', function() {
+          var comp = 
+      webpackJsonp([4],{
+
+/***/ "./pages/index.js":
+/***/ (function(module, exports, __webpack_require__) {
+
+[1, 2, 3].map(function (x) {
+  var y = x + 1;
+  return x * y;
+});
+    (function (Component, route) {
+      if(!Component) return
+      if (false) return
+      module.hot.accept()
+      Component.__route = route
+
+      if (module.hot.status() === 'idle') return
+
+      var components = next.router.components
+      for (var r in components) {
+        if (!components.hasOwnProperty(r)) continue
+
+        if (components[r].Component.__route === route) {
+          next.router.update(r, Component)
+        }
+      }
+    })(typeof __webpack_exports__ !== 'undefined' ? __webpack_exports__.default : (module.exports.default || module.exports), "/")
+  
+
+/***/ }),
+
+/***/ 3:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("./pages/index.js");
+
+
+/***/ })
+
+},[3])
+          return { page: comp.default }
+        })
+      ;
+//# sourceMappingURL=index.js.map
