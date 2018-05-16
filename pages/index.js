e1a4d9b2540ab7f5a4a84988563a68a28ead9bb6
ruben <rubenbustos33@gmail.com>
2018-05-15 21:15:05 -0400

    eslint setup

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
-[1, 2, 3].map((x) => {
-  const y = x + 1;
-  return x * y;
-});
+// Testing ESLint
+// To disable Eslint on a line of code, add the following code to that line:
+// eslint-disable-line
 
+
+// To disable Eslint inside a file, add the following code to the top of that file:
+/* eslint-disable */
+
+
+
+// [1, 2, 3].map((x) => {
+//   const y = x + 1;
+//   return x * y;
+// });
+
+// foo(reallyLongArg(), reallyReallyLongArg(), omgSoManyParameters(), IShouldRefactorThis(), isThereSeriouslyAnotherOne());
