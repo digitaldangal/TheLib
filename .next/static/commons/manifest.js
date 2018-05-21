d87e86f956e8a6afcbf61d48c61cc0bfd600d2d4
ruben <rubenbustos33@gmail.com>
2018-05-20 21:06:55 -0400

    Pass user prop to index

--- .next/static/commons/manifest.js
+++ .next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "bfa7da88e49360b866e7"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "c079a9ad6a476a49c18d"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_document.js","2":"bundles/pages/_error.js","3":"bundles/pages/_app.js"}[chunkId]||chunkId) + "-" + {"0":"17fc7edc50a25d65fc86","1":"433554d5516b550a4ac4","2":"99988b845fbb92323b21","3":"623caac567f9b1515e77"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_document.js","2":"bundles/pages/_error.js","3":"bundles/pages/_app.js","5":"bundles/pages/index.js"}[chunkId]||chunkId) + "-" + {"0":"17fc7edc50a25d65fc86","1":"489d849a5c5d4a4ce4b9","2":"99988b845fbb92323b21","3":"623caac567f9b1515e77","5":"71fe390d77d826b695d0"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,32 @@
+import PropTypes from 'prop-types';
+
 import Head from 'next/head';
 
 import withLayout from '../lib/withLayout';
 
-const Index = () => (
+const Index = ({ user }) => (
   <div style={{ padding: '10px 45px' }}>
     <Head>
       <title>Index page</title>
       <meta name="description" content="This is SEO description of Index page" />
     </Head>
     <p>Content on Index page</p>
+    <p>Email: {user.email}</p>
   </div>
 );
+
+// Static method that passes data to a page by populating the props of my component
+Index.getInitialProps = async ({ query }) => ({ user: query.user });
+
+// Passing user prop to Index
+Index.propTypes = {
+  user: PropTypes.shape({
+    email: PropTypes.string.isRequired,
+  }),
+};
+
+Index.defaultProps = {
+  user: null,
+};
 
 export default withLayout(Index);
