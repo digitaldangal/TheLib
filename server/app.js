c2fed9bd9472ec90074b1bc79e0add8af61d5535
ruben <rubenbustos33@gmail.com>
2018-05-22 12:33:14 -0400

    pass user prop to the Index page aka Dashboard

--- server/app.js
+++ server/app.js
@@ -59,10 +59,11 @@ app.prepare().then(() => {
   };
   server.use(session(sess));
 
-  server.get('/', async (req, res) => {
-    req.session.foo = 'bar';
-    const user = await User.findOne({ slug: 'devdad' });
-    app.render(req, res, '/', { user });
+  server.get('/', (req, res) => {
+    User.findOne({ slug: 'devdad' }).then((user) => {
+      req.user = user;
+      app.render(req, res, '/');
+    });
   });
 
   server.get('*', (req, res) => handle(req, res));
