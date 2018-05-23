665f26735ff8ae4fa3b85542b3818062ac9feef0
ruben <rubenbustos33@gmail.com>
2018-05-23 13:44:45 -0400

    Write signInOrSignUp static method for users

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,6 @@
+import _ from 'lodash';
 import mongoose from 'mongoose';
-
+import generateSlug from '../utils/slugify';
 // const Schema = mongoose.Schema;
 const { Schema } = mongoose;
 
@@ -45,12 +46,66 @@ const mongoSchema = new Schema({
   },
 });
 
+class UserClass {
+  static publicFields() {
+    return [
+      'id',
+      'displayName',
+      'email',
+      'avatarUrl',
+      'slug',
+      'isAdmin',
+      'isGithubConnected',
+    ];
+  }
+
+  static signInOrSignUp({
+    googleId, email, googleToken, displayName, avatarUrl,
+  }) {
+    return this.findOne({ googleId }, UserClass.publicFields().join(' ')).then((user) => {
+      if (user) {
+        const modifier = {};
+
+        if (googleToken.accessToken) {
+          modifier.access_token = googleToken.accessToken;
+        }
+
+        if (googleToken.refreshToken) {
+          modifier.refresh_token = googleToken.refreshToken;
+        }
+
+        if (isEmpty(modifier)) {
+          return Promise.resolve(user);
+        }
+
+        return this.updateOne({ googleId }, { $set: modifier }).then(() => Promise.resolve(user));
+      }
+
+      const userCount = this.find().count();
+
+      return generateSlug(displayName).then(slug =>
+        this.create({
+          createdAt: new Date(),
+          googleId,
+          email,
+          googleToken,
+          displayName,
+          avatarUrl,
+          slug,
+          isAdmin: userCount === 0,
+        }).then(newUser => Promise.resolve(pick(newUser, UserClass.publicFields()))));
+    });
+  }
+}
+
+mongoSchema.loadClass(UserClass);
 const User = mongoose.model('User', mongoSchema);
 
 export default User;
 
 
 // TODO:
-// integrate Google OAuth []
-// integrate Github Auth []
+// integrate Google OAuth
+// integrate Github Auth
+// Switch from Promise.then to async/await
 
